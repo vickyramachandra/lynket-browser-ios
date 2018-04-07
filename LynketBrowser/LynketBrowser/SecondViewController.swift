f1d3227b860f9bef2f4e07754f1c2652932060dc
Vignesh Ramachandra <vickyramachandra@gmail.com>
2018-04-07 18:26:40 +0530

    Created Xcode project - Added TabBar controller

---
+++ LynketBrowser/LynketBrowser/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LynketBrowser
+//
+//  Created by Vignesh Ramachandra on 07/04/18.
+//  Copyright © 2018 Vignesh Ramachandra. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
